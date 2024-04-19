e12b03477523316531b58a2ee2792267af91ba5d
eldosz <144575101+eldosz@users.noreply.github.com>
2024-04-18 18:33:54 -0700

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -3,29 +3,15 @@ resource "aws_security_group" "allow_tls" {
   description = "Allow TLS inbound traffic"
   vpc_id     = aws_vpc.main.id
 
-  ingress {
+  dynamic ingress {
     description      = "TLS from VPC"
-    from_port        = var.ports[0]
-    to_port          = var.ports[0]
+    from_port        = ingress.value.from_port
+    to_port          = ingress.value.from_port
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
-    ingress {
-    description      = "TLS from VPC"
-    from_port        = var.ports[1]
-    to_port          = var.ports[1]
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
 
-    ingress {
-    description      = "TLS from VPC"
-    from_port        = var.ports[2]
-    to_port          = var.ports[2]
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
 
   egress {
     from_port        = 0
